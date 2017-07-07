378ae714952341c9493e8f74c2f654de55eeeaef
徐嘉俊 <xujiajun@hrsoft.net>
2017-07-07 15:56:43 +0800

    【story=5】Banner 完成部分
    Change-Id: I2b49322d65fc227e12edd0040ab45b892f70dd61

--- src/routes/about/route.js
+++ src/routes/about/route.js
@@ -1,8 +1,8 @@
 module.exports = {
   path: 'about',
-  childRoutes: [
-    require('./info/route'),
-  ],
+  // childRoutes: [
+  //   require('./info/route'),
+  // ],
   getComponents(nextState, callback){
     require.ensure([], () => {
       callback(null, require('./index'))
