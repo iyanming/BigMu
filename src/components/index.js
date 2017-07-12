8e8291aac11a4700ab2bfa3cc036bf304e1b6b45
周宝 <zhoubao@hrsoft.net>
2017-07-12 19:39:08 +0000

    【story = 39】将联系我们路由绑定到了已经写好的页面上
    Change-Id: Id4aaec6cf3fec23b0f77e485c9f84b5690854052

--- src/components/index.js
+++ src/components/index.js
@@ -2,17 +2,10 @@
  * Created by out_xu on 17/7/3.
  */
 
-// import Header from './Header'
-// import Footer from './Footer'
-// import SubNav from './SubNav'
-// import PageBanner from './PageBanner'
-// import Markdown from './Markdown'
-//
-// export { Header, Footer, SubNav, PageBanner, Markdown }
 import Header from './Header'
 import Footer from './Footer'
 import SubNav from './SubNav'
 import PageBanner from './PageBanner'
 import Markdown from './Markdown'
 
-export { Header, Footer, SubNav, PageBanner }
+export { Header, Footer, SubNav, PageBanner, Markdown }
